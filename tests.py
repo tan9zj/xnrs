1e883cdbdfae09a5a7aad31e6da8becec44c225b
lucasmllr <lucasmoeller@me.com>
2024-08-19 17:24:33 +0200

    updating tests

--- tests.py
+++ tests.py
@@ -8,24 +8,20 @@ from train import train
 from xnrs.explain import MindExplainer
 
 
-def test_training():
-    print('testing mind training')
+def test_training(config_path: PathLike = 'config/mind_standard.yml'):
+    print('testing config: ', config_path)
+    train(cfg_path=config_path, debug=True)
+    print('seems fine')
     print('-------')
-    train(cfg_path='config/mind_standard.yml', debug=True)
-    print('seems fine\n')
-    print('testing adressa training')
-    print('-------')
-    train(cfg_path='config/mind_standard.yml', debug=True)
-    print('seems fine\n')
 
 
 def test_init_mind_explainer(
-    model_path: PathLike = '../../experiments/mind_standard_test_0/checkpoints/ckpt_hack',
-    news_path: PathLike = '/mount/arbeitsdaten/tcl/data/mind/MINDlarge_dev/news_full_sbert.pkl',
-    user_path: PathLike = '/mount/arbeitsdaten/tcl/data/mind/MINDlarge_dev/behaviors.csv',
+    model_path: PathLike = 'checkpoints/xnrs_mind_checkpoint',
+    news_path: PathLike = '../data/mind/MINDlarge_dev/news_full_sbert.pkl',
+    user_path: PathLike = '../data/mind/MINDlarge_dev/behaviors.csv',
     device: Optional[str] = None
 ):
-    print('\ntesting explainer initialization')
+    print('testing explainer initialization')
     assert os.path.exists(model_path)
     assert os.path.exists(news_path)
     assert os.path.exists(user_path)
@@ -39,16 +35,18 @@ def test_init_mind_explainer(
         user_path=user_path
     )
     print('seems fine')
+    print('-------')
 
 
 def test_imports():
-    print('\ntesting imports')
+    print('testing imports')
     import xnrs
     from xnrs import MindExplainer
     from xnrs import BCELogitsRankingTrainer, BCERankingTrainer, MSERankingTrainer
     from xnrs.data import AdressaHandler, NewsRecDataset, make_mind_data
     from xnrs.models import make_model
     print('seems fine')
+    print('-------')
 
 
 def test_adressa_dataset(cfg_path: PathLike = 'config/adressa_standard.yml'):
@@ -59,9 +57,9 @@ def test_adressa_dataset(cfg_path: PathLike = 'config/adressa_standard.yml'):
     cfg = DotMap(cfg)
     print('init datasets')
     train_ds, test_ds = AdressaHandler.init_datasets(cfg)
-    print('init successful, dataset sizes:')
+    print('init successful, train and test set sizes:')
     print(len(train_ds), len(test_ds))
-    return 
+    print('-------')
 
 
 def test_mind_dataset(cfg_path: PathLike = 'config/mind_standard.yml'):
@@ -72,15 +70,16 @@ def test_mind_dataset(cfg_path: PathLike = 'config/mind_standard.yml'):
     cfg = DotMap(cfg)
     print('init datasets')
     train_ds, test_ds = make_mind_data(cfg)
-    print('init successful, dataset sizes:')
+    print('init successful, train and test set sizes:')
     print(len(train_ds), len(test_ds))
-    return 
+    print('-------')
 
 
 if __name__ == '__main__':
 
-    # test_imports()
-    # test_init_mind_explainer()
-    # test_training()
-    # test_adressa_dataset()
+    test_imports()
+    test_adressa_dataset()
     test_mind_dataset()
+    test_training(config_path='config/adressa_standard.yml')
+    test_training(config_path='config/mind_standard.yml')
+    test_init_mind_explainer()
